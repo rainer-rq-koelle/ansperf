c9e64767e856c6809dbf322fa889d4a837f1bdf7
Rainer Koellee <rainer.koelle@eurocontrol.int>
2024-02-27 19:49:30 +0100

    testing infrastructure and pipeline with dummy function xhello_world()

---
+++ R/xhelloworld.R
@@ -0,0 +1,12 @@
+#' dummy test function
+#'
+#' @param .name addressee name for the greeting
+#'
+#' @return
+#' @export
+#'
+#' @examples
+xhello_world <- function(.name){
+  greeting <- paste0("Hello dear Padawan ", .name)
+  return(greeting)
+}
---
+++ tests/testthat/test-xhelloworld.R
@@ -0,0 +1,3 @@
+test_that("hello world", {
+  expect_equal(xhello_world("Rainer"), "Hello dear Padawan Rainer")
+})
